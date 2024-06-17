bff615df4e09a565e56320477401d92af681e0ac
Lucas Hillys <lucashillysz@gmail.com>
2024-06-17 15:00:00 -0400

    Criado Order e OrderStatus

---
+++ Intermed/Program.cs
@@ -0,0 +1,32 @@
+﻿using Intermed.Entities.Enums;
+using Intermed.Entities;
+using System;
+
+
+namespace Intermed
+{
+    internal class Program
+
+    {
+        private static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1800,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+
+
+        }
+    }
+}
